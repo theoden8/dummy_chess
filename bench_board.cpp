d4f8f697a4ca38831a35aa2bb58d7a3e1246ee7a
Kirill Rodriguez <theoden8@tutamail.com>
2021-01-08 16:33:52 +0000

    displaying shannon number: it doesnt add up!

--- bench_board.cpp
+++ bench_board.cpp
@@ -9,6 +9,9 @@ int main() {
   printf("fixed depth benchmarks\n");
   printf("\n");
   printf("starting position\n");
+  constexpr uint64_t shannon_number[] = {
+    20, 400, 8902, 197281, 4865609, 119060324, 3195901860
+  };
   for(int depth = 1; depth <= 5; ++depth) {
     Engine e(fen::starting_pos);
     auto start = system_clock::now();
@@ -17,7 +20,8 @@ int main() {
     long dur = duration_cast<nanoseconds>(stop-start).count();
     double sec = 1e-9*dur;
     double ndssec = double(e.nodes_searched)/sec;
-    printf("depth=%d, move=%s, time=%.3f\t%.3f kN/sec\n", depth, board::_pos_str(m).c_str(), sec, ndssec/1e3);
+    printf("depth=%d, move=%s, time=%.3f\t%.3f kN/sec\tnodes=%lu\tshannon=%lu\n",
+            depth, board::_pos_str(m).c_str(), sec, ndssec/1e3, e.nodes_searched, shannon_number[depth-1]);
   }
   return 0;
 }
