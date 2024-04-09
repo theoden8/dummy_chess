2894a69b8cb89a28a74c88842428371ff9281e48
Kirill Rodriguez <theoden8@tutamail.com>
2024-04-09 21:33:05 +0100

    wrapped fen class

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,4 +6,41 @@ extern crate libc;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-use root::*;
+use root::{fen,FFIString};
+
+#[no_mangle] // Ensure the Rust compiler does not mangle the name of this function
+pub fn to_str(c_str_ptr: *const std::os::raw::c_char) -> String {
+    unsafe {
+        // Convert the raw C string to a `CStr`
+        let c_str = std::ffi::CStr::from_ptr(c_str_ptr);
+
+        // Convert the `CStr` to a Rust `String`. If the C string is not valid UTF-8,
+        // this will replace invalid sequences with � (REPLACEMENT CHARACTER).
+        // To handle errors differently, use `c_str.to_str()` and match on the result.
+        c_str.to_string_lossy().into_owned()
+    }
+}
+
+pub struct FEN {
+  fen: fen::FEN,
+}
+
+impl FEN {
+  pub fn new(s: &str) -> FEN {
+    let c_str = s.as_ptr() as *const _;
+    unsafe {
+      let cpp_str = FFIString::make_string(c_str);
+      let fen = fen::FEN_load_from_string(&cpp_str);
+      FEN { fen }
+    }
+  }
+
+  pub fn str(&self) -> String {
+    unsafe {
+      let cpp_str = self.fen.export_as_string();
+      let c_str = FFIString::to_cstring(&cpp_str);
+      to_str(c_str)
+    }
+  }
+}
+
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,48 +1,11 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+extern crate dummy_chess;
 
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-use root::*;
-
-#[no_mangle] // Ensure the Rust compiler does not mangle the name of this function
-pub fn to_str(c_str_ptr: *const std::os::raw::c_char) -> String {
-    unsafe {
-        // Convert the raw C string to a `CStr`
-        let c_str = std::ffi::CStr::from_ptr(c_str_ptr);
-
-        // Convert the `CStr` to a Rust `String`. If the C string is not valid UTF-8,
-        // this will replace invalid sequences with � (REPLACEMENT CHARACTER).
-        // To handle errors differently, use `c_str.to_str()` and match on the result.
-        c_str.to_string_lossy().into_owned()
-    }
-}
-
+use dummy_chess::*;
 
 fn main() {
   println!("MAIN");
-  let mut s: String = "".to_string();
-  unsafe {
-//    let dummychess = dummychess::new(env!("OUT_DIR"));
-//    println!("{}", dummychess.fen);
-    println!("starting pos cstr1");
-    let starting_pos_cstr1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".as_ptr() as *const _;
-    println!("starting pos str1");
-    let starting_pos_str1 = FFIString::make_string(starting_pos_cstr1);
-    println!("starting pos fen");
-    let starting_pos_fen = fen::FEN_load_from_string(&starting_pos_str1);
-    println!("starting pos str2");
-    let starting_pos_str2 = starting_pos_fen.export_as_string();
-    println!("starting pos str show");
-    FFIString::show_cstring(&starting_pos_str2);
-    println!("starting pos cstr2");
-    let starting_pos_cstr2 = FFIString::to_cstring(&starting_pos_str2);
-    println!("to str");
-    s = to_str(starting_pos_cstr2);
-    //let board: root::Board = Board::new(&starting_pos_fen as *const _, 1 << 19);
-  }
-  println!("fen: {}", s);
+  let starting_pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  let fen: FEN = FEN::new(starting_pos);
+  println!("fen: {}", fen.str());
   println!("EXIT");
 }
